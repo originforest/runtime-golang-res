1f77ba732944d1f66d2ad23965d0d996e6bc1e62
fiathux Su <moyu.su@fiathuxdeMacBook-Pro.local>
2021-01-26 17:33:46 +0800

    [feature] extra context map

--- model/context.go
+++ model/context.go
@@ -17,14 +17,15 @@ import (
 
 // A ContextRT contain FaaS event meta-datas
 type ContextRT struct {
-	FnID       string      // function ID
-	ResidentID string      // resident ID
-	RequestID  string      // current request ID
-	ResToken   string      // cloud resource token
-	Method     string      // method
-	EventName  string      // event source name
-	Resource   string      // event resource path
-	Host       string      // host name of event trigger's
-	Query      url.Values  // query data in event URI
-	Header     http.Header // event headers
+	FnID       string                 // function ID
+	ResidentID string                 // resident ID
+	RequestID  string                 // current request ID
+	ResToken   string                 // cloud resource token
+	Method     string                 // method
+	EventName  string                 // event source name
+	Resource   string                 // event resource path
+	Host       string                 // host name of event trigger's
+	Query      url.Values             // query data in event URI
+	Header     http.Header            // event headers
+	Extra      map[string]interface{} // extra values
 }
